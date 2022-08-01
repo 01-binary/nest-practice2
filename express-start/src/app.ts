f459de946708fc995b7ecfcbd725bf248bff239e
jinsoo <admin@binary01.me>
2022-08-01 20:51:52 +0900

    feat: 야옹 end

--- express-start/src/app.ts
+++ express-start/src/app.ts
@@ -3,26 +3,48 @@
 import * as express from 'express';
 import catsRouter from './cats/cats.route';
 
-const app: express.Express = express();
+class Server {
+  public app: Express.Application;
 
-//* logging middleware
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log('this is logging middleware');
-  next();
-});
+  constructor() {
+    const app: Express.Application = express();
+    this.app = app;
+  }
 
-//* json middleware
-app.use(express.json());
+  private setRoute() {
+    this.app.use(catsRouter);
+  }
 
-app.use(catsRouter);
+  private setMiddleware() {
+    //* logging middleware
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log('this is logging middleware');
+      next();
+    });
 
-//* 404 middleware
-app.use((req, res, next) => {
-  console.log('this is error middleware');
-  res.send({ error: '404 not found error' });
-});
+    //* json middleware
+    this.app.use(express.json());
+    this.setRoute();
 
-app.listen(8000, () => {
-  console.log('server is on...');
-});
+    //* 404 middleware
+    this.app.use((req, res, next) => {
+      console.log('this is error middleware');
+      res.send({ error: '404 not found error' });
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(8000, () => {
+      console.log('server is on...');
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
