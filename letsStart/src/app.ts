b23e935ee98abecd5330387e49e3662dfee23502
jinsoo <admin@binary01.me>
2022-08-01 19:52:59 +0900

    feat

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,1 +1,12 @@
-console.log("hello world!!");
+import * as express from 'express';
+
+const app: express.Express = express();
+const port: number = 8000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => {
+  console.log('server start');
+});
