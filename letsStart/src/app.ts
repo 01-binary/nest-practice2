6095515e0dda943cec573aeef788d1f384930820
jinsoo <admin@binary01.me>
2022-08-01 20:00:28 +0900

    feat: 냐옹

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,12 +1,36 @@
 import * as express from 'express';
+import { Cat, CatType } from './app.model';
 
 const app: express.Express = express();
-const port: number = 8000;
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('hello world');
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log('this is logging middleware');
+  next();
 });
 
-app.listen(port, () => {
-  console.log('server start');
+app.get('/cats/som', (req, res, next) => {
+  console.log('this is som middleware');
+  next();
+});
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send({ cats: Cat });
+});
+
+app.get('/cats/blue', (req, res, next: express.NextFunction) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get('/cats/som', (req, res) => {
+  res.send({ som: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log('this is error middleware');
+  res.send({ error: '404 not found error' });
+});
+
+app.listen(8000, () => {
+  console.log('server is on...');
 });
