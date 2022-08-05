8f803a31ab7ad62dd2e540ca4f1a52f2091b9453
jinsoo <admin@binary01.me>
2022-08-06 01:34:41 +0900

    feat: 예외처리ㅣㅣㅣ드아ㅏㅏㅏㅏㅏㅏ 냐오오ㅗ옹

--- nest-start/src/cats/cats.controller.ts
+++ nest-start/src/cats/cats.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
@@ -6,12 +15,15 @@ export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getAllCat() {
+    throw new HttpException('api 18', 401);
     return 'all cat';
   }
 
   @Get(':id')
-  getOneCat() {
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
     return 'one cat';
   }
 
