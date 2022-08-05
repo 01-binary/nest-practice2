7b35878b4bf750ee58bfac4b6d6da61f3ab8de18
jinsoo <admin@binary01.me>
2022-08-05 23:44:39 +0900

    모오듈

---
+++ nest-start/src/cats/cats.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+}
