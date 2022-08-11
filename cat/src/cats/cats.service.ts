a425ab2483aaea69bef564b939d9e95f3efdd54c
jinsoo <admin@binary01.me>
2022-08-11 22:08:23 +0900

    feat: repository pattern

--- cat/src/cats/cats.service.ts
+++ cat/src/cats/cats.service.ts
@@ -4,14 +4,15 @@ import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { Cat } from './cat.schema';
 import { CatRequestDto } from './dto/cats.request.dto';
+import { CatsRepository } from './cats.repository';
 
 @Injectable()
 export class CatsService {
-  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  constructor(private readonly catRepository: CatsRepository) {}
 
   async signUp(body: CatRequestDto) {
     const { email, name, password } = body;
-    const isCatExist = await this.catModel.exists({ email });
+    const isCatExist = await this.catRepository.existsByEmail(email);
 
     if (isCatExist) {
       throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
@@ -19,7 +20,7 @@ export class CatsService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const cat = await this.catModel.create({
+    const cat = await this.catRepository.create({
       email,
       name,
       password: hashedPassword,
